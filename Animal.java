3d0092aef5b0fa10e91d043ec5e0cc59146c4378
lailar27 <laishirod07@gmail.com>
2025-03-06 20:48:22 -0800

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,53 +1,57 @@
-package dennisMohle.myZoo.com;
+// Animal.java
+// lR 3/6/25
+
+package lailaRodriguez.myZoo.com;
 
 public class Animal {
 
-    // Animal Class attributes
     private String animalName;
     private String species;
     private int age;
 
-    // Create a static attribute that belongs to the Animal class.
+    // Static attribute belonging to Animal class
+
     public static int numOfAnimals = 0;
 
-    // Animal Class constructors.
-    public Animal(String name, String aSpecies, int anAge) {
+    // Constructors belonging to Animal Class
+
+    public Animal(String name, String aSpecies, int anAge){
         System.out.println("\n A new Animal object was created.\n");
 
-        // Create initial values for the class attributes.
-        animalName = name;
+        // Initial values for the class attributes.
+        animalName =  name;
         species = aSpecies;
         age = anAge;
         numOfAnimals++;
     }
 
-    public Animal() {
+    public Animal(){
         System.out.println("\n A new Animal object was created.\n");
         numOfAnimals++;
     }
 
-    // Getters and Setters for each attribute (optional, but recommended for good practice)
-    public String getName() {
+    // Getters and Setters for each attribute
+    public String getName(){
         return animalName;
     }
 
-    public void setName(String name) {
+    public void setName(String name){
         this.animalName = name;
     }
 
-    public int getAge() {
+    public int getAge(){
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age){
         this.age = age;
     }
 
-    public String getSpecies() {
+    public String getSpecies(){
         return species;
     }
 
-    public void setSpecies(String species) {
+    public void setSpecies(String species){
         this.species = species;
     }
 
--- App.java
+++ App.java
@@ -1,4 +1,7 @@
-package dennisMohle.myZoo.com;
+// App.java
+// lR 3/6/25
+
+package lailaRodriguez.myZoo.com;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,9 +9,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("\n\n Welcome to My Zoo Program\n\n");
-        System.out.println("\n Number of animals is: " + Animal.numOfAnimals);
+    public static void main(String [] args){
+        System.out.println("\n\n Welcome to My Zoo Program!\n\n");
+        System.out.println("\n The number of animals is: " + Animal.numOfAnimals);
 
         // local variables
         String name;
@@ -18,34 +21,33 @@ public class App {
         // ArrayList of Animal objects
         ArrayList<Animal> animals = new ArrayList<>();
 
-        // Open an external file, parse it line by line, and get age and species
-        String filePath = "C:/2024_Spring/midtermFiles/arrivingAnimals.txt";
+        // Open external file > parse it line by line > get age and species
+        String filePath = "C:/Users/LailaRodriguez/IntelliJ/MidtermPractice/arrivingAnimals.txt";
         File file = new File(filePath);
 
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
+        try (Scanner scanner = new Scanner(file)){
+            while (scanner.hasNextLine()){
                 String line = scanner.nextLine();
 
-                // Age is in the first element of the array named parts
                 String[] parts = line.split(", ");
 
-                // Check if the line has at least 1 part
-                if (parts.length >= 1) {
+                // Check if line has at least one part
+                if (parts.length >= 1){
                     String ageAndSpecies = parts[0];
                     System.out.println("ageAndSpecies: " + ageAndSpecies );
 
                     // Get age out of 'ageAndSpecies'
-                    String[] theParts = ageAndSpecies.split(" ");
-                    for (int i=0; i<5; i++) {
+                    String[] theParts = ageAndSpecies.split("");
+                    for (int i=0; i<5; i++ ){
                         System.out.println("theParts[" + i + "] is " + theParts[i]);
                     }
                     age = Integer.parseInt(theParts[0]);
                     species = theParts[4];
 
-                    // Create a new animal object.
-                    Animal myAnimal = new Animal("name needed", species, age);
+                    // Create a new animal object
+                    Animal myAnimal = new Animal ("name needed", species, age);
 
-                    // Add the new Animal object to the ArrayList of Animals
+                    // Add new Animal object to the ArrayList of Animals
                     animals.add(myAnimal);
                 }
                 System.out.println("\n Number of animals is: " + Animal.numOfAnimals);
@@ -55,10 +57,10 @@ public class App {
             e.printStackTrace();
         }
 
-        // We now have an arrayList of Animals. Let's output them!
+        // Output arrayList of Animals!
         for (Animal animal : animals){
             System.out.println(animal);
-            System.out.println("Animal name: " + animal.getName() + ", Age: " + animal.getAge() + ", Species: " + animal.getSpecies());
+            System.out.println("Animal name: " + animal.getName() +  ", Age: " + animal.getAge() + ", Species: " + animal.getSpecies());
         }
         System.out.println("\n Number of animals is: " + Animal.numOfAnimals);
 
